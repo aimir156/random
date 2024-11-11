9f9696de8a7d50270d8c4c9e698e09d36e5ba83d
aimir156 <aimirkurmanbekov@icloud.com>
2024-11-11 20:44:41 +0600

    Random

---
+++ Fetch.js
@@ -0,0 +1,30 @@
+// console.log('Шгаг 1');
+// setTimeout(() => {
+//     console.log('Шаг 2 (через 2 секунды)');
+// }, 0);
+// console.log('Шаг 3');
+
+// let promise = new Promise((resolve, reject) => {
+//     let user = {
+//         name: 'Ivan'
+//     }
+//     setTimeout(() =>{
+//         resolve(1)
+//     }, 2000)
+//     resolve(user)
+//     if(false){
+//         reject('Ошибка')
+//     }
+// })
+// Promise.then((result) => console.log(result)
+// )
+
+// async function f(){
+//     let promise = new Promise((resolve, reject) => {
+//         setTimeout(() => resolve('готово'), 1000)
+//     });
+//     let result = await promise;
+//     alert(result)
+// }
+// f();
+
